237a50e8f039175982f7f4b69fd873957d393c69
Admin <Admin@Admin-PC>
2018-09-01 23:09:17 +0530

    first commit

---
+++ src/main/java/com/example/akwebservice/AkWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.akwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkWebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/akwebservice/AkWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.akwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
